8398240af80aab16dcbce372567f981b1a936b75
iamakashsoni <akash.soni.3101@gmail.com>
2025-03-23 01:10:51 +0530

    first commit

---
+++ sumOfNaturalNumbers.js
@@ -0,0 +1,13 @@
+// Question 1: sum of all natural numbers from 1 to n
+
+function sumOfNaturalNumber(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(sumOfNaturalNumber(5));
+console.log(sumOfNaturalNumber(10));
+console.log(sumOfNaturalNumber(3));
